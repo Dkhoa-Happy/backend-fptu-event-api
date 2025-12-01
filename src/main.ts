da0e4db9fbcbc846f05c6b7482459cc391562efe
Dang Khoa <dangkhoa13978@gmail.com>
2025-12-01 20:22:02 +0700

    Initialize database schema with User and Event tables, including foreign key constraints and unique index on email. Update Swagger documentation path from 'api/doc' to 'api/docs'.

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/doc', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 8080);
 }
